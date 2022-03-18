1cd68d2a7e7f2169fc56330155a14f3a80c0d7f0
Damon <damondias@gmail.com>
2022-03-18 10:47:02 -0400

    feat: structure project

---
+++ database_LaBoleria/scripts/001-cria-banco.sql
@@ -0,0 +1,44 @@
+CREATE TABLE cakes (
+	"id" serial NOT NULL,
+	"name" varchar NOT NULL,
+	"price" numeric NOT NULL,
+	"image" varchar NOT NULL,
+	"description" TEXT NOT NULL,
+	CONSTRAINT "cakes_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE clients (
+	"id" serial NOT NULL,
+	"name" varchar NOT NULL,
+	"adress" varchar NOT NULL,
+	"phone" varchar NOT NULL,
+	CONSTRAINT "clients_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE orders (
+	"id" serial NOT NULL,
+	"clientId" integer NOT NULL,
+	"cakeId" integer NOT NULL,
+	"quantity" integer NOT NULL,
+	"createdAt" DATE NOT NULL DEFAULT 'NOW()',
+	"totalPrice" numeric NOT NULL,
+	CONSTRAINT "orders_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "orders" ADD CONSTRAINT "orders_fk0" FOREIGN KEY ("clientId") REFERENCES "clients"("id");
+ALTER TABLE "orders" ADD CONSTRAINT "orders_fk1" FOREIGN KEY ("cakeId") REFERENCES "cakes"("id");
+
