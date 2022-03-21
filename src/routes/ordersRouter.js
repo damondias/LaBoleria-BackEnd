2e4cd16cc251b9f71c8df80ea0418112f028c97f
Damon <damondias@gmail.com>
2022-03-20 20:03:14 -0400

    feat: GET orders by id

--- src/routes/ordersRouter.js
+++ src/routes/ordersRouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import validateSchemasMiddleware from '../middlewares/validateSchemasMiddleware.js';
 import orderSchema from '../schemas/orderSchema.js';
-import { postOrders, getOrders } from '../controllers/ordersController.js';
+import { postOrders, getOrders, getOrdersById } from '../controllers/ordersController.js';
 
 const ordersRouter = express.Router();
 
 ordersRouter.post('/order', validateSchemasMiddleware(orderSchema), postOrders);
 ordersRouter.get('/orders', getOrders);
-// ordersRouter.get('/orders/:id', getOrdersById);
+ordersRouter.get('/orders/:id', getOrdersById);
 
 
 
